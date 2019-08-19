c3bc58fc74c85350559e8eb0ef7803bf6cc8b314
T-rex007 <shaqc777@yahoo.com>
2019-08-19 13:38:16 -0400

    ----

--- VisionUtils.py
+++ VisionUtils.py
@@ -121,3 +121,15 @@ def report(results, n_top=3): ### Go through and understand
 def feat_distance_cosine_scalar(feat1, feat2):
     similarity = np.dot(feat1 , feat2) / (np.linalg.norm(feat1) * np.linalg.norm(feat2))
     return similarity
+
+def increase_brightness(img, value=30):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+
+    lim = 255 - value
+    v[v > lim] = 255
+    v[v <= lim] += value
+
+    final_hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img
